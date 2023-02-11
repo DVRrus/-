119129dd8ea61974b462e576986c38762f11ad2b
Данил Валеев <danil010404@mail.ru>
2023-02-11 11:32:56 +0500

    5

---
+++ часы/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace часы
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(596, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(386, 426);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(421, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(423, 167);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(86, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(420, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(418, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Название продукта";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(446, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Цена";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(607, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Дата изготовки";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(596, 88);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(134, 20);
+            this.dateTimePicker1.TabIndex = 11;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(421, 225);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(88, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ часы/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace часы
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       // string connStr = "server=192.168.25.23;port=44444;user=st_1_20_6;database=is_1_20_st6_KURS;password=40112334;";
+        string connStr = "server=chuc.caseum.ru;port=33333;user=st_1_20_6;database=is_1_20_st6_KURS;password=40112334;";
+        //Переменная соединения
+           //string connStr = "server=10.90.12.110;port=33333;user=st_1_20_6;database=is_1_20_st6_KURS;password=40112334;";
+        MySqlConnection conn;
+        public void SetMyCustomFormat()
+        {
+            // Set the Format type and the CustomFormat string.
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "YYYY-MM-MM";
+        }
+        public string data1(DateTimePicker k)
+        {
+           var DataTime1 = dateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss");
+            return DataTime1.ToString();
+        } 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Введите мне имя", "Ошибка");
+                return;
+
+            }
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("Какова моя цена?", "Ошибка");
+                return;
+
+            }
+
+            if (dateTimePicker1.Text == "")
+            {
+                MessageBox.Show("Дата изготовки", "Ошибка");
+                return;
+
+            }
+            string ca = data1(dateTimePicker1);
+
+            MySqlCommand command = new MySqlCommand($"INSERT INTO Eda (Name,Price,Data) VALUES('@Name', @Price,'{ca}');", conn);
+
+            conn.Open();
+            command.Parameters.Add("@Name", MySqlDbType.VarChar, 25).Value = textBox1.Text;
+            command.Parameters.Add("@Price", MySqlDbType.Float, 25).Value = textBox2.Text;
+            // command.Parameters.Add("@Data", MySqlDbType.DateTime, 25).Value =ca;
+            //     adapter.SelectCommand = command;
+            //  adapter.Fill(table);
+            try
+            {
+                if (command.ExecuteNonQuery() == 1)
+                {
+                   
+                    MessageBox.Show("Вы успешно добавили");
+                    GetListUsers();
+                }
+                else
+                {
+
+                    MessageBox.Show("Произошла ошибка");
+                }
+            }
+            catch(Exception ex)
+            {
+               
+                MessageBox.Show("Ошибка" + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            
+
+        }
+      
+        DataTable table = new DataTable();
+        //Объявляем адаптер
+        MySqlDataAdapter adapter = new MySqlDataAdapter();
+        //Объявляем команду
+        private BindingSource bSource = new BindingSource();
+
+
+        // string connStr = "server=10.90.12.110;port=33333;user=st_1_20_6;database=is_1_20_st6_KURS;password=40112334;";
+        //Метод обновления DataGreed
+       
+        public void GetListUsers()
+        {
+            dataGridView1.DataSource = default;
+            //Запрос для вывода строк в БД
+
+            string commandStr = $"SELECT * FROM Eda";
+            conn = new MySqlConnection(connStr);
+            //Открываем соединение
+
+            conn.Open();
+            
+            //Объявляем команду, которая выполнить запрос в соединении conn
+            adapter.SelectCommand = new MySqlCommand(commandStr, conn);
+            //Заполняем таблицу записями из БД
+            adapter.Fill(table);
+            //Указываем, что источником данных в bindingsource является заполненная выше таблица
+            bSource.DataSource = table;
+            //Указываем, что источником данных ДатаГрида является bindingsource 
+            dataGridView1.DataSource = bSource;
+            //Закрываем соединение
+            conn.Close();
+            //Отражаем количество записей в ДатаГриде
+            int count_rows = dataGridView1.RowCount - 1;
+            label1.Text = (count_rows).ToString();
+
+        }
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // строка подключения к БД
+            // string connStr = "server=10.90.12.110;port=33333;user=st_1_20_6;database=is_1_20_st6_KURS;password=40112334;";
+         //   string connStr = "server=chuc.caseum.ru;port=33333;user=st_1_20_6;database=is_1_20_st6_KURS;password=40112334;";
+            // создаём объект для подключения к БД
+              conn = new MySqlConnection(connStr);
+            //Вызываем метод для заполнение дата Грида
+           GetListUsers();
+            //Видимость полей в гриде
+            dataGridView1.Columns[0].Visible = true;
+            dataGridView1.Columns[1].Visible = true;
+            dataGridView1.Columns[2].Visible = true;
+            dataGridView1.Columns[3].Visible = true;
+          
+
+            //Ширина полей
+            dataGridView1.Columns[0].FillWeight = 15;
+            dataGridView1.Columns[1].FillWeight = 40;
+            dataGridView1.Columns[2].FillWeight = 15;
+            dataGridView1.Columns[3].FillWeight = 15;
+        
+            //Режим для полей "Только для чтения"
+            dataGridView1.Columns[0].ReadOnly = true;
+            dataGridView1.Columns[1].ReadOnly = true;
+            dataGridView1.Columns[2].ReadOnly = true;
+            dataGridView1.Columns[3].ReadOnly = true;
+           
+            //Растягивание полей грида
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+          
+            //Убираем заголовки строк
+            dataGridView1.RowHeadersVisible = false;
+            //Показываем заголовки столбцов
+            dataGridView1.ColumnHeadersVisible = true;
+            //Вызываем метод покраски ДатаГрид
+            //ChangeColorDGV();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /*  private void ChangeColorDGV()
+          {
+              //Отражаем количество записей в ДатаГриде
+              int count_rows = dataGridView1.RowCount - 1;
+              label1.Text = (count_rows).ToString();
+              DateTime now = DateTime.Now;
+
+              //Проходимся по ДатаГриду и красим строки в нужные нам цвета, в зависимости от статуса студента
+              for (int i = 0; i < count_rows; i++)
+              {
+
+                  //статус конкретного студента в Базе данных, на основании индекса строки
+                  bool id_selected_status = Convert.ToBoolean(dataGridView1.Rows[i].Cells[3].Value);
+                  //Логический блок для определения цветности
+                  if (id_selected_status = DateTime)
+                  {
+                      //Красим в красный
+                      dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Red;
+                  }
+                  if (id_selected_status = DateTime) 
+                  {
+                      //Красим в зелёный
+                      dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Green;
+                  }
+                  if (id_selected_status = DateTime)
+                  {
+                      //Красим в желтый
+                      dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Cyan;
+                  }
+              }
+          }*/
+
+    }
+}
